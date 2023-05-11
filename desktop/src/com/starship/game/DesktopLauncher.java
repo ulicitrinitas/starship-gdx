4d55bee12208671e51706439511fc50701e3f8ff
citrinitas <uli.citrinitas@gmail.com>
2023-05-11 16:52:58 -0300

    initial commit

---
+++ desktop/src/com/starship/game/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.starship.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.starship.game.StarShip;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+		config.setForegroundFPS(60);
+		config.setTitle("SpaceShip::Game::Java::GDX");
+		config.setWindowedMode(1280, 720);
+
+		new Lwjgl3Application(new StarShip(), config);
+	}
+}
