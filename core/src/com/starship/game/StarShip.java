fc08ad4ec2b2eaa7a1ea57893d9fd5e91c0b39ef
citrinitas <uli.citrinitas@gmail.com>
2023-05-12 20:47:03 -0300

    using bg.png as background

--- core/src/com/starship/game/StarShip.java
+++ core/src/com/starship/game/StarShip.java
@@ -12,7 +12,7 @@ public class StarShip extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("bg.png");
 	}
 
 	@Override
