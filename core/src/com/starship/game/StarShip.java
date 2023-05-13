29ee20c2fc5beae237880f9fb3d1ab99272aac03
citrinitas <uli.citrinitas@gmail.com>
2023-05-13 03:25:04 -0300

    add sprite to ship

--- core/src/com/starship/game/StarShip.java
+++ core/src/com/starship/game/StarShip.java
@@ -4,16 +4,19 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class StarShip extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img, tShip;
+	private Sprite ship;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("bg.png");
 		tShip = new Texture("spaceship.png");
+		ship = new Sprite(tShip);
 	}
 
 	@Override
@@ -21,7 +24,7 @@ public class StarShip extends ApplicationAdapter {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
-		batch.draw(tShip, 0, 0);
+		batch.draw(ship, 15, 15);
 		batch.end();
 	}
 	
